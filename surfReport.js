a2d8f5db3a1ac7aa0b4acec6731b8c6b45b6348a
bbalconi <bbalconi@gmail.com>
2017-09-25 15:28:04 -0600

    surfReport.js has been modified

--- surfReport.js
+++ surfReport.js
@@ -1,12 +1,17 @@
 var surfObj = {
-    date: new Date(),
-    todaysWaves:"tubular",
-    processReport: function(){
-       //edits go here
-    }
+  date: new Date(),
+  todaysWaves:"tubular",
+  processReport: function(){
+    var dateStr = surfObj.date.toLocaleDateString('en-US');
+    return "Today is " + dateStr + ", and the waves are " + surfObj.todaysWaves + ", dude.";
+  }
 }
 
 //Using the "date" and "todaysWaves" properties, edit the function "processReport" 
 //to create and console.log a string that logs "today is {date}
 //and the waves are {todaysWaves}, dude." The date should formatted as a "short date."
 surfObj.processReport();
+
+//console.log(surfObj.date, surfObj.todaysWaves, surfObj.processReport());
+
+module.exports = surfObj;
---
+++ surfReport.test.js
@@ -0,0 +1,35 @@
+var surfReport = require('./surfReport.js');
+var chai = require('chai');
+chai.should();
+var assert = chai.assert;
+var expect = chai.expect;
+
+//Using the "date" and "todaysWaves" properties, edit the function "processReport" 
+//to create and console.log a string that logs "today is {date}
+//and the waves are {todaysWaves}, dude." The date should formatted as a "short date."
+
+describe.only("surfReport", function(){
+  it ('can report current waves', function(){
+    var val = surfReport.processReport();
+    val.should.be.a('string');
+  });
+  it ('says dude', function(){
+    var val = surfReport.processReport();
+    expect(val).to.include(', dude.');
+  });
+  it ('says tubular', function(){
+    var val = surfReport.processReport();
+    expect(val).to.include('and the waves are tubular');
+  });
+  it ('says today is 12/25/2017', function(){
+    surfReport.date = new Date("12/25/2017");
+    var val = surfReport.processReport();
+    expect(val).to.include('12/25/2017');
+  });
+  it ('says today is 3/17/2018', function(){
+    surfReport.date = new Date("3/17/2018");
+    var val = surfReport.processReport();
+    expect(val).to.include('3/17/2018');
+  });
+});
+
