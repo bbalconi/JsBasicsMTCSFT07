7218b0ca3235443ef58952e29ccbf27678cc50e3
bbalconi <bbalconi@gmail.com>
2017-09-25 16:30:42 -0600

    added sweet css, especially border, among other things

--- surfReport.js
+++ surfReport.js
@@ -1,11 +1,11 @@
-var surfObj = {
+var  surfObj = {
   date: new Date(),
   todaysWaves:"tubular",
   processReport: function(){
     var dateStr = surfObj.date.toLocaleDateString('en-US');
     return "Today is " + dateStr + ", and the waves are " + surfObj.todaysWaves + ", dude.";
   }
-}
+};
 
 //Using the "date" and "todaysWaves" properties, edit the function "processReport" 
 //to create and console.log a string that logs "today is {date}
