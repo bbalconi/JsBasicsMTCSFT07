bfd9ec26c0dc24234f2a2caa9cda7d633e698d1e
bbalconi <bbalconi@gmail.com>
2017-09-26 15:40:39 -0600

    updated html-practice.html

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 var express = require("express");
- var app = express();
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json({ type: 'application/json'}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'))
+app.use(function(req, res, next){
+  res.setHeader('Content-Type', 'application/json');
+  next();
+});
+var allNames = ['Moby', 'Burger']
 
- /* serves main page */
- app.get("/", function(req, res) {
-    res.sendfile('index.htm')
- });
+/* serves main page */
+app.get("/", function(req, res) {
+  res.sendfile('html-practice.html')
+});
 
- var port = process.env.PORT || 5000;
- app.listen(port, function() {
-   console.log("Listening on " + port);
- });
+app.get("/names", function(req, res){
+  res.json(allNames);
+});
+
+app.post("/names", function(req, res){
+  console.log(req.body.name);
+  allNames.push(req.body.name)
+  res.json('body received ' + req.body.name);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
